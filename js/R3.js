a0658ed19aa46a7798d51b7434df08b69ae8ca82
pedromaximilian <pedro.maximilian@gmail.com>
2021-05-26 21:00:28 -0300

    Moved all to code folder

--- js/R3.js
+++
@@ -1,8 +0,0 @@
-
-
-$('#videoModal').on('hidden.bs.modal', function (e) {
-    $(this).find('video')[0].pause(); //pause video on modal close
- })
- $('#videoModal').on('shown.bs.modal', function (e) {
-    $(this).find('video')[0].play(); //play video on modal show
- })
